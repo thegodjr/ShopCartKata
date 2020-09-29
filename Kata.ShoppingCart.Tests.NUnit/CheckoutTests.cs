15cf46944938fcc4ac03877f6be1b500bff01ccf
Dmitry Bolshakov <thegodjr@outlook.com>
2020-09-29 21:21:16 +0300

    Implement ICheckout and add tests

--- Kata.ShoppingCart.Tests.NUnit/CheckoutTests.cs
+++ Kata.ShoppingCart.Tests.NUnit/CheckoutTests.cs
@@ -11,10 +11,48 @@ namespace Kata.ShoppingCart.Tests.NUnit
         }
 
         [Test]
-        public void Checkout_GetTotal_WhenNotImplemented_ShouldThrow()
+        public void Checkout_GetTotal_EachSkus_ShouldPass()
         {
             var checkout = new Checkout();
-            Assert.Throws<NotImplementedException>(() => checkout.GetTotal("A"));
+            var total = checkout.GetTotal("ABCD");
+
+            Assert.AreEqual(115, total);
+        }
+
+        [Test]
+        public void Checkout_GetTotal_3A_ShouldHaveDiscounts()
+        {
+            var checkout = new Checkout();
+            var total = checkout.GetTotal("AAA");
+
+            Assert.AreEqual(130, total);
+        }
+
+        [Test]
+        public void Checkout_GetTotal_2B_ShouldHaveDiscounts()
+        {
+            var checkout = new Checkout();
+            var total = checkout.GetTotal("BB");
+
+            Assert.AreEqual(45, total);
+        }
+
+        [Test]
+        public void Checkout_GetTotal_2C_ShouldNotHaveDiscounts()
+        {
+            var checkout = new Checkout();
+            var total = checkout.GetTotal("CC");
+
+            Assert.AreEqual(40, total);
+        }
+
+        [Test]
+        public void Checkout_GetTotal_4A2Bcd_ShouldHaveDiscounts()
+        {
+            var checkout = new Checkout();
+            var total = checkout.GetTotal("ADABCABA");
+
+            Assert.AreEqual(260, total);
         }
     }
 }
--- Kata.ShoppingCart/Checkout.cs
+++ Kata.ShoppingCart/Checkout.cs
@@ -1,12 +1,64 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
 
 namespace Kata.ShoppingCart
 {
     public class Checkout : ICheckout
     {
+        private Dictionary<char, decimal> skuPrices = new Dictionary<char, decimal>
+        {
+            { 'A', 50 },
+            { 'B', 30},
+            { 'C', 20},
+            { 'D', 15},
+        };
+
+        private Dictionary<char, Tuple<int, decimal>> skuDiscounts = new Dictionary<char, Tuple<int, decimal>>
+        {
+            { 'A', new Tuple<int, decimal> (3, 130) },
+            { 'B', new Tuple<int, decimal> (2, 45) }
+        };
+
         public double GetTotal(string items)
         {
-            throw new NotImplementedException();
+            var skusCount = new Dictionary<char, Tuple<int, decimal, decimal>>();
+            foreach (var item in items)
+            {
+                decimal skuPrice;
+                if (skuPrices.TryGetValue(item, out skuPrice))
+                {
+                    Tuple<int, decimal, decimal> skuCount;
+                    if (skusCount.TryGetValue(item, out skuCount))
+                    {
+                        skusCount[item] = new Tuple<int, decimal, decimal>(skuCount.Item1 + 1, skuCount.Item2 + skuPrice, skuPrice);
+                    }
+                    else
+                    {
+                        skusCount[item] = new Tuple<int, decimal, decimal>(1, skuPrice, skuPrice);
+                    }
+                }
+            }
+
+            decimal sum = 0;
+            foreach (var item in skusCount)
+            {
+                Tuple<int, decimal> skuDiscount;
+
+                if (skuDiscounts.TryGetValue(item.Key, out skuDiscount))
+                {
+                    int skusCountWithoutDiscounts;
+                    var multiplicator = Math.DivRem(item.Value.Item1, skuDiscount.Item1, out skusCountWithoutDiscounts);
+
+                    sum += multiplicator * skuDiscount.Item2 + skusCountWithoutDiscounts * item.Value.Item3;
+                }
+                else
+                {
+                    sum += item.Value.Item2;
+                }
+            }
+
+            return (double)sum;
         }
     }
 }
